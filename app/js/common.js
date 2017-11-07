2f90149ad125e2fac660d7ea03dbae4b5a37ac3c
Arkadiy Nazin <arcan.run@gmail.com>
2017-11-08 00:21:50 +0300

    added self-made -js gallery

--- app/js/common.js
+++ app/js/common.js
@@ -9,39 +9,39 @@ $(function() {
 
 // owl
 $('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    center:true,
-    nav: true,
-    navText: ["<i class='fa fa-chevron-left' aria-hidden='true'></i>", "<i class='fa fa-chevron-right' aria-hidden='true'></i>"],
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:3
-        },
-        1000:{
-            items:5
-        }
+  loop:true,
+  margin:10,
+  center:true,
+  nav: true,
+  navText: ["<i class='fa fa-chevron-left' aria-hidden='true'></i>", "<i class='fa fa-chevron-right' aria-hidden='true'></i>"],
+  responsive:{
+    0:{
+      items:1
+    },
+    600:{
+      items:3
+    },
+    1000:{
+      items:5
     }
+  }
 })
 // end owl
 
 // tabs
 $(".tabs-content .item-delivery").not(":first").hide();
 $(".tabs-control a").on("click", function(e){
-   e.preventDefault();
-   $(".tabs-control a").removeClass("active").eq($(this).index()).addClass("active");
-   $(".tabs-content .item-delivery").hide().eq($(this).index()).fadeIn();
-   console.log($(this).index());
-   console.log($(this).eq());
+ e.preventDefault();
+ $(".tabs-control a").removeClass("active").eq($(this).index()).addClass("active");
+ $(".tabs-content .item-delivery").hide().eq($(this).index()).fadeIn();
+ console.log($(this).index());
+ console.log($(this).eq());
 });
 // end tabs
 
 // tooltip
 $(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip();   
+  $('[data-toggle="tooltip"]').tooltip();   
 });
 // end tooltip
 
@@ -55,41 +55,77 @@ $(".aside-mnu li>a").on("click", function(e){
 // end aside mnu
 
 // range slider
-    $( "#slider-range" ).slider({
-      range: true,
-      min: 0,
-      max: 100000,
-      values: [ 0, 100000 ],
-      slide: function( event, ui ) {
-        $( "#amount_1" ).val(  ui.values[ 0 ] );
-        $( "#amount_2" ).val(  ui.values[ 1 ] );
-      }
-    });
-    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
-      " - $" + $( "#slider-range" ).slider( "values", 1 ) );
+$( "#slider-range" ).slider({
+  range: true,
+  min: 0,
+  max: 100000,
+  values: [ 0, 100000 ],
+  slide: function( event, ui ) {
+    $( "#amount_1" ).val(  ui.values[ 0 ] );
+    $( "#amount_2" ).val(  ui.values[ 1 ] );
+  }
+});
+$( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+  " - $" + $( "#slider-range" ).slider( "values", 1 ) );
 
-    $( "#amount_1" ).on("keypress", function(){
-      if($("#amount_1").val() <= $( "#slider-range" ).slider( "values", 1 ) )
-      {
-        $( "#slider-range" ).slider( "values",0, $(this).val() );
+$( "#amount_1" ).on("keypress", function(){
+  if($("#amount_1").val() <= $( "#slider-range" ).slider( "values", 1 ) )
+  {
+    $( "#slider-range" ).slider( "values",0, $(this).val() );
 
-      }
-     
-    });
-    $( "#amount_2").on("keypress", function(){
-      if($("#amount_2").val() > $("#amount_1").val())
-      {
-        $( "#slider-range" ).slider( "values",1, $(this).val() );
-      }
-     
-    });
+  }
+
+});
+$( "#amount_2").on("keypress", function(){
+  if($("#amount_2").val() > $("#amount_1").val())
+  {
+    $( "#slider-range" ).slider( "values",1, $(this).val() );
+  }
+
+});
 // end range slider
-
+// toggle show more filter
 $("#showFilter1").on("click", function(e){
   e.preventDefault();
   $(".drop-down-filter-body").fadeToggle();
   $(".more-filter-2").toggleClass("bg-darker");
   
 });
-     
+// end toggle filter
+  // on min imgs
+  var index = 1;
+  $(".min-gallery").on("click", function(e){
+    e.preventDefault();
+    index = $(this).children("img").attr("alt");
+    $(".max-gallery").attr("src", "img/ipone-card-" + index + ".png")
+    console.log(index);
+  });
+  // end om min imgs
+  // on arrows
+  
+  // to the right
+  $(".min-right").on("click", function(){
+    if(index <= 2)
+    {
+      var i = ($(this).siblings("a").next().index()) - 2;
+      index = +index + i;
+      $(".max-gallery").attr("src", "img/ipone-card-" + index + ".png")
+      console.log(index);
+    }
+  });
+  // to the left
+  $(".min-left").on("click", function(){
+    if(index >= 2)
+    {
+     var i = ($(this).siblings("a").prev().index()) - 2;
+     index = +index + i;
+     $(".max-gallery").attr("src", "img/ipone-card-" + index + ".png")
+     console.log(index);
+   }
+ });
+  
+  // end om arrows
+// mini gallary for card page
+
+// end mini gallary for card page
 });
